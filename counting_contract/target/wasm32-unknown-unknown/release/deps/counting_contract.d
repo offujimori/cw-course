1b1014e811df4123f3ca05b3115023cfc04e53d6
off <bus.fujimori@gmail.com>
2024-04-11 21:34:59 -0300

    updating

---
+++ counting_contract/target/wasm32-unknown-unknown/release/deps/counting_contract.d
@@ -0,0 +1,7 @@
+/home/off/Desktop/git/cw-course/counting_contract/target/wasm32-unknown-unknown/release/deps/counting_contract.wasm: src/lib.rs
+
+/home/off/Desktop/git/cw-course/counting_contract/target/wasm32-unknown-unknown/release/deps/libcounting_contract.rlib: src/lib.rs
+
+/home/off/Desktop/git/cw-course/counting_contract/target/wasm32-unknown-unknown/release/deps/counting_contract.d: src/lib.rs
+
+src/lib.rs:
---
+++ counting_contract/target/wasm32-unknown-unknown/release/libcounting_contract.d
@@ -0,0 +1,1 @@
+/home/off/Desktop/git/cw-course/counting_contract/target/wasm32-unknown-unknown/release/libcounting_contract.rlib: /home/off/Desktop/git/cw-course/counting_contract/src/lib.rs
